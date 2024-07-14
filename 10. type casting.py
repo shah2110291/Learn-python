273b092fa686f84995c228022f6b373d70442db5
shah21102 <shahkhalid21102@gmail.com>
2024-07-15 03:23:36 +0500

    files are added

---
+++ 10. type casting.py
@@ -0,0 +1,21 @@
+print("This Program is about the types conversion")
+
+a=12
+b=32.423
+new_number = a+b
+
+# printing the Number and there Data Type
+print("The New value is =", new_number)
+print("The Data Type =" ,type(new_number))
+
+# Types Casting program
+
+x=21 
+y= '984'
+
+# Explicit Type Casting
+y= int(y)
+Second_number= x+y
+# printing result and there type
+print("The Secone New_Number =", Second_number)
+print("The Type =", type(Second_number))

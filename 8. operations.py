2d43819dac67413442c95148ffda694e2c370a14
shah21102 <shahkhalid21102@gmail.com>
2024-07-15 02:42:10 +0500

    files are added

---
+++ 8. operations.py
@@ -0,0 +1,22 @@
+print("These are all opertions in pythone")
+
+a=201
+b=32
+
+sum=a+b
+print("The sum of these two are:", sum)
+
+div=a/b
+print("The division :", div)
+
+multiplication= a*b
+print("The multiplication:", multiplication)
+
+sqrt=a**b
+print("The Squre Root = ", sqrt)
+
+Modulo= a%b
+print("The Reminder =" ,Modulo)
+
+floor_Division = a// b
+print("Floor Division = ", floor_Division)
---
+++ 9. opertions taking inouts from user.py
@@ -0,0 +1,24 @@
+print("These are all operations in Python")
+
+# Take inputs from the user and convert them to integers or floats
+a = float(input("Enter First Number = "))
+b = float(input("Enter Second Number = "))
+
+# Perform operations
+sum_result = a + b
+print("The sum of these two are:", sum_result)
+
+div = a / b
+print("The division:", div)
+
+multiplication = a * b
+print("The multiplication:", multiplication)
+
+sqrt = a ** b
+print("The power (a raised to b):", sqrt)
+
+Modulo = a % b
+print("The remainder:", Modulo)
+
+floor_Division = a // b
+print("Floor Division:", floor_Division)

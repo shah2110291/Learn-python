1a80523b98e233492df032164a85612987075928
shah21102 <shahkhalid21102@gmail.com>
2024-07-17 02:35:08 +0500

    file is added

---
+++ 12. strings in python.py
@@ -0,0 +1,14 @@
+name="M shah khalid"
+
+a_letter= name[0]
+short_name= name[0:5] 
+reverse_order =name[-11:-6] 
+ 
+print(a_letter)
+print(short_name)
+print(reverse_order)
+
+
+print(name[:5]) #same is = print(name[0:5])
+print(name[1:]) #same is = print(name[1:13])
+print(name [2: 10 :2])
